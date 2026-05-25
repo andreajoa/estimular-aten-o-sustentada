ec89270e51a9a41dcf5b182e92ea12dd4f635726
Andre Almeida <contact@andre-almieda.online>
2026-05-24 23:47:42 -0300

    fix: restaura homepage profissional e mantém paginas conectadas

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import Hero from "./components/Hero";
-import Activities from "./components/Activities";
-import Games from "./components/Games";
-import Progress from "./components/Progress";
-import Tips from "./components/Tips";
+import HomeClient from "./components/HomeClient";
 
 export default function Home() {
-  return (
-    <main>
-      <Hero />
-      <Activities />
-      <Games />
-      <Progress />
-      <Tips />
-    </main>
-  );
+  return <HomeClient />;
 }
